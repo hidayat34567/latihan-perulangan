a375499061ad97e25fc8ba8f12ae3ce59768844a
hidayat34567 <2223622.wafa@smkn-2sbg.sch.id>
2024-02-13 11:05:33 +0700

    latihan-perulangan

---
+++ perulangan foreach.php
@@ -0,0 +1,115 @@
+<?php
+$books = [
+    "Panduan Belajar PHP untuk Pemula",
+    "Membangun Aplikasi Web dengan PHP",
+    "Tutorial PHP dan MySQL",
+    "Membuat Chat Bot dengan PHP"
+];
+
+echo "<h5>Judul Buku PHP:</h5>";
+echo "<ul>";
+foreach($books as $buku){
+    echo "<li>$buku</li>";
+}
+echo "</ul>";
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>DATA DIRI WAFA HABIBAH HIDAYAT</title>
+    <style>
+      body {
+        background-color: #e8e8e8;
+      }
+      
+      .kartu {
+        width: 450px;
+        margin: 0 auto;
+        margin-top: 70px;
+            box-shadow: 0 0.25rem 0.75rem rgba(0,0,0,.03);
+    transition: all .3s;
+           background-color: #591869;
+    border: solid 8px #ea92ff;
+    border-top-right-radius: 80px;
+    border-bottom-left-radius: 80px;
+      } 
+      .kartu:hover {
+        background-color: #1f8a45;
+        border: solid 8px #4fd47e;
+        border-top-left-radius: 80px;
+    border-bottom-right-radius: 80px;
+    border-top-right-radius: 0px;
+    border-bottom-left-radius: 0px;
+      }
+      .foto {
+            padding: 20px;
+    margin-left: 20px;
+    margin-top: 10px;
+      }
+      tbody {
+    font-size: 20px;
+    font-weight: 200;
+    color:white;
+}
+.biodata {
+    margin-top: 30px;
+}
+    </style>
+  </head>
+  <body>
+        <a href="https://wafa habibah.com/"><h3 class="text-center text-success mt-3">DATA DIRI WAFA HABIBAH HIDAYAT</h3></a>
+        <!-- Isi Biodata Paling Keren di HTML disini -->
+<div class="container">
+  <div class="card kartu">
+    <div class="row">
+      <div class="col-md-4">
+      <div class="foto">
+      <td rowspan="10" align="center"><img src="habibah.jpg" width="100" height="150"></td>
+      </div>
+      </div>
+      <div class="col-md-8 kertas-biodata">
+        <div class="biodata">
+        <table width="100%" border="0">
+    <tbody><tr>
+        <td valign="top">
+        <table border="0" width="100%" style="padding-left: 2px; padding-right: 13px;">
+          <tbody>
+            <tr>
+              <td width="25%" valign="top" class="textt">Nama</td>
+                <td width="2%">:</td>
+                <td style="color: #e9a7f9; font-weight:bold">Wafa Habibah Hidayat</td>
+            </tr>
+          <tr>
+              <td class="textt">Jenis Kelamin</td>
+                <td>:</td>
+                <td>Perempuan</td>
+            </tr>
+          <tr>
+              <td class="textt">Tempat Lahir</td>
+                <td>:</td>
+                <td>Purwakarta,11-November-2006</td>
+            </tr>
+          <tr>
+              <td class="textt">Jurusan</td>
+                <td>:</td>
+                <td>Rekayasa Perangkat Lunak</td>
+            </tr>
+        </tbody></table>
+        </td>
+    </tr>
+    </tbody></table>
+  </div>
+      </div>
+    </div>
+  </div>
+</div>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
